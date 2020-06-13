7952e014904642f2c1cd0203b8c0faf17667d5bb
Kevin Hassenkamp <khassenkamp@gmail.com>
2020-06-13 14:04:31 -0500

    first commit

---
+++ build/precache-manifest.5d586fa0b2bce1dcc34b014a27675dd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0113bda0b21f736ec771c9603a38aee5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a66ec86e933bce201d2",
+    "url": "/static/css/main.e3b7a8e2.chunk.css"
+  },
+  {
+    "revision": "057822a738ef70c344fc",
+    "url": "/static/js/2.3daa1c74.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3daa1c74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a66ec86e933bce201d2",
+    "url": "/static/js/main.5f97ecd8.chunk.js"
+  },
+  {
+    "revision": "16385764b937c4287c41",
+    "url": "/static/js/runtime-main.0f4090cb.js"
+  }
+]);
